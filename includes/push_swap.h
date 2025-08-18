715eedd13b261a73e6b1a77fedc63cceea5710bc
asando <alexando17@yahoo.com>
2025-08-18 19:08:15 +0200

    add declaration for flag in push_swap.h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 18:13:16 by asando            #+#    #+#             */
-/*   Updated: 2025/08/17 18:21:04 by asando           ###   ########.fr       */
+/*   Updated: 2025/08/18 19:06:02 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,33 @@
 # include "libft.h"
 # include "ft_printf.h"
 
-typedef struct s_stack 
+typedef struct s_stack
 {
 	int				value;
 	struct s_stack	*next;
 	struct s_stack	*prev;
 }	t_stack;
 
+typedef enum e_swap_flag
+{
+	SA = 1 << 0,
+	SB = 1 << 1,
+	SS = 1 << 2
+}	t_swap_flag;
+
+typedef enum e_push_flag
+{
+	PA = 1 << 0,
+	PB = 1 << 1
+}	t_push_flag;
+
 //main function
-int	grab_arg(int n_arg, char **s, t_stack **stack);
+int		grab_arg(int n_arg, char **s, t_stack **stack);
 
 //stack utils function
 t_stack	*stack_new(int content);
-int	stack_add_back(int content, t_stack **stack);
 void	stack_add_front(int content, t_stack **stack);
 void	stack_clean(t_stack **stack);
+int		stack_add_back(int content, t_stack **stack);
+
 #endif
--- main.c
+++ main.c
@@ -6,32 +6,39 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 17:14:57 by asando            #+#    #+#             */
-/*   Updated: 2025/08/17 21:04:02 by asando           ###   ########.fr       */
+/*   Updated: 2025/08/18 16:01:11 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	push_swap(t_stack **stack_1, t_stack **stack_2)
+{
+
+	return ;
+}
+
 int	main(int argc, char **argv)
 {
-	//int	i;
 	t_stack	*stack_1;
+	t_stack	*stack_2;
 	t_stack	*curr;
 
-	//i = 1;
 	stack_1 = NULL;
+	stack_2 = NULL;
 	if (grab_arg(argc, argv, &stack_1) == 0)
 	{
-		curr = stack_1;
-		while (curr)
-		{
-			ft_printf("%d\n", curr->value);
-			curr = curr->next;
-		}
+		push_swap(&stack_1, &stack_2);
+		//curr = stack_1;
+		//while (curr)
+		//{
+		//	ft_printf("%d\n", curr->value);
+		//	curr = curr->next;
+		//}
 	}
 		//do a push swap algorithm
-	if (stack_1)
+	if (stack_1) //maybe we can clean two stack in one function
 		stack_clean(&stack_1);
-	//ft_printf("Error\n");
+	// Error will lounch error massage in fd 2 STDERR
 	return (0);
 }
