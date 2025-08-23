f31444a8808cd8d7a6c2e3d1b5f6d3160cb0eebb
asando <alexando17@yahoo.com>
2025-08-23 16:53:48 +0200

    deleting unessesary code from push.c

--- main.c
+++ main.c
@@ -6,17 +6,11 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 17:14:57 by asando            #+#    #+#             */
-/*   Updated: 2025/08/21 17:55:56 by asando           ###   ########.fr       */
+/*   Updated: 2025/08/23 16:50:01 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-//void	push_swap(t_stack **stack_1, t_stack **stack_2)
-//{
-//
-//	return ;
-//}
 
 static void	print_stack(t_stack **stack_1, t_stack **stack_2)
 {
@@ -32,15 +26,18 @@ static void	print_stack(t_stack **stack_1, t_stack **stack_2)
 		curr = curr->next;
 		i++;
 	}
-	arr_1[i] = curr->value;
+	if (*stack_1)
+		arr_1[i] = curr->value;
 	curr = *stack_2;
 	i = 0;
 	while (curr && curr->next != *stack_2)
 	{
 		arr_2[i] = curr->value;
 		curr = curr->next;
+		i++;
 	}
-	//arr_2[i] = curr->value;
+	if (*stack_2)
+		arr_2[i] = curr->value;
 	i = 0;
 	while (i < 10)
 	{
@@ -63,7 +60,10 @@ int	main(int argc, char **argv)
 		//push_swap(&stack_1, &stack_2);
 		print_stack(&stack_1, &stack_2);	
 		//swap_ps(&stack_1, &stack_2, SS);
-		push_ps(&stack_1, &stack_2, PB);
+		push(&stack_1, &stack_2, PB);
+		push(&stack_1, &stack_2, PB);
+		swap(&stack_1, &stack_2, SS);
+		rotate(&stack_1, &stack_2, RR);
 		print_stack(&stack_1, &stack_2);	
 		//{
 		//	ft_printf("%d\n", curr->value);
