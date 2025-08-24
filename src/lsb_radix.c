3f2aa36928f8c9145875fd8fb6503f2e2fafad6f
asando <alexando17@yahoo.com>
2025-08-24 09:54:17 +0200

    creating merge_sort algorithm to do backend preparation for lsb_radix, note: not yet following nourminette

--- src/lsb_radix.c
+++ src/lsb_radix.c
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 16:59:59 by asando            #+#    #+#             */
-/*   Updated: 2025/08/24 08:47:46 by asando           ###   ########.fr       */
+/*   Updated: 2025/08/24 09:50:49 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,67 @@ static int	*store_to_array(t_stack **stack)
 	return (arr_value);
 }
 
-static int	merge_sort(int *arr)
+static int	merge_arr(int *arr, int left, int mid, int right)
 {
+	int	i;
+	int	j;
+	int	k;
+	int	*left_arr;
+	int	*right_arr;
 
+	i = 0;
+	j = 0;
+	k = 0;
+	left_arr = malloc(sizeof(int) * (mid - left + 1));
+	right_arr = malloc(sizeof(int) * (right - mid));
+	if (left_arr == NULL || right_arr == NULL)
+	{
+		free(left_arr);
+		free(right_arr);
+		return (-1);
+	}
+	while (i < (mid - left + 1))
+	{
+		left_arr[i] = arr[left + i];
+		i++;
+	}
+	while (j < (right- mid))
+	{
+		right_arr[j] = arr[mid + 1 + j];
+		j++;
+	}
+	i = 0;
+	j = 0;
+	while (i < (mid - left + 1) && j < (right - mid))
+	{
+		if (left_arr[i] < right_arr[j])
+			arr[left + k++] = left_arr[i++];
+		else
+			arr[left + k++] = right_arr[j++];
+	}
+	while (i < (mid - left + 1))
+		arr[left + k++] = left_arr[i++];
+	while (j < (right - mid))
+		arr[left + k++] = right_arr[j++];
+	free(left_arr);
+	free(right_arr);
+	return (0);
+}
+
+static int	merge_sort(int *arr, int left, int right)
+{
+	int	mid;
+
+	mid = (right - left) / 2;
+	if (left < right)
+	{
+		if (merge_sort(arr, left, mid) == -1)
+			return (-1);
+		if (merge_sort(arr, mid + 1, right) == -1)
+			return (-1);
+		if (merged_arr(arr, left, mid, right) == -1)
+			return (-1);
+	}
 	return (0);
 }
 
