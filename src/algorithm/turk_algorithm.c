c107d2a4b76c45e45b2300e7c38abb651a82570d
asando <alexando17@yahoo.com>
2025-09-12 21:46:46 +0200

    wrote turk_algorithm_utils

--- src/algorithm/turk_algorithm.c
+++ src/algorithm/turk_algorithm.c
@@ -6,63 +6,62 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 15:33:57 by asando            #+#    #+#             */
-/*   Updated: 2025/09/12 18:37:53 by asando           ###   ########.fr       */
+/*   Updated: 2025/09/12 21:44:44 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.c"
+#include "push_swap.h"
+
+t_stack	*max_value(t_stack **stack)
+{
+	t_stack	*max;
+	t_stack	*current;
+
+	current = *stack;
+	max = *stack;
+	while (current->next != *stack)
+	{
+		if (current->value > max->value)
+			max = current;
+		current = current->next;
+	}
+	if (current->value > max->value)
+		max = current;
+	return (max);
+}
 
 static void	sort_three(t_stack **stack_1, t_stack **stack_2)
 {
-	t_stack *current;
+	t_stack	*current;
 
 	current = *stack;
-	if (max_nbr(stack_1) == *stack_1)
+	if (max_value(stack_1) == *stack_1)
 		rotate(stack_1, stack_2, RA);
-	else if (max_nbr(stack_1) == (*stack)->next)
+	else if (max_value(stack_1) == (*stack)->next)
 		reverse_rotate(stack_1, stack_2, RRA);
 	if ((*stack)->value > (*stack)->next->value)
 		swap(stack_1, stack_2, SA);
 	return ;
 }
 
-static t_stack *max_nbr(t_stack **stack)
-{
-	t_stack *max;
-	t_stack *current;
-
-	current = *stack;
-	max = *stack;
-	while (current->next != *stack)
-	{
-		if (current->value > current->next->value)
-			max = current;
-		else
-			max = current->next->value;
-		current = current->next;
-	}
-	return (max);
-}
-
 static void	sort_stack(t_stack **stack_1, t_stack **stack_2)
 {
 	size_t	len_stack;
 
-	len_stack = stack_size(*stack_1);
+	len_stack = stack_size(stack_1);
 	if (len_stack-- > 3 && check_sorted_list(stack_1))
 		push(stack_1, stack_2, PA);
 	if (len_stack-- > 3 && check_sorted_list(stack_1))
 		push(stack_1, stack_2, PA);
 	while (len_stack-- > 3 && check_sorted_list(stack_1))
 	{
-		init_content_stack(stack_1, stack_2, len_stack);
-
+		init_stack(stack_1, stack_2);
 	}
 	sort_three(stack_1, stack_2, );
 }
 
 void	turk_algo(t_stack **stack_1, t_stack **stack_2)
-{ 
+{
 	size_t	len_stack;
 
 	len_stack = stack_size(*stack_1);
--- src/algorithm/turk_algorithm_utils.c
+++ src/algorithm/turk_algorithm_utils.c
@@ -6,39 +6,36 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 18:12:15 by asando            #+#    #+#             */
-/*   Updated: 2025/09/12 18:54:38 by asando           ###   ########.fr       */
+/*   Updated: 2025/09/12 21:45:54 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	init_content_stack(t_stack **stack_1, t_stack **stack_2,
-						size_t len_stack, int direction)
+#include "push_swap.h"
+
+void	init_stack(t_stack **stack_1, t_stack **stack_2, int direction)
 {
+	set_median_index(stack_1);
+	set_median_index(stack_2);
 	if (direction == 0)
 	{
-		set_median_index(stack_1);
 		set_target(stack_1, stack_2);
 		set_push_cost(stack_2);
-		set_cheapest(stack_1);
-
 	}
-	else //the other way around from b to a change the parameter
-	{
-		set_median_index(stack_1);
+	else
 		set_target(stack_1, stack_2);
-		set_push_cost(stack_2);
-		set_cheapest(stack_1);
-	}
 	return ;
 }
 
-static void	set_median_index(t_stack **stack_1, size_t len_stack)
+static void	set_median_index(t_stack **stack)
 {
 	size_t	median;
+	size_t	len_stack;
 	t_stack	*current;
 	int		i;
 
 	i = 0;
-	current = *stack_1;
+	current = *stack;
+	len_stack = stack_size(stack);
 	median = len_stack / 2;
 	while (current->next != *stack)
 	{
@@ -80,9 +77,56 @@ static void	set_target_a(t_stack **stack_1, t_stack **stack_2)
 			current_a->target = current_b;
 		}
 		if (target_value == LONG_MIN)
-			current_a->target = max_nbr(stack_2);
+			current_a->target = max_value(stack_2);
 		current_b = current_b->next;
 		current_a = current_a->next;
 	}
 	return ;
+}
+
+static void	set_push_cost(t_stack **stack_1, t_stack **stack_2)
+{
+	size_t	len_stack_1;
+	size_t	len_stack_2;
+	t_stack	*current;
+
+	current = *stack_1;
+	len_stack_1 = stack_size(stack_1);
+	len_stack_2 = stack_size(stack_2);
+	while (current->next != *stack_1)
+	{
+		current->push_cost = current->current_index;
+		if (current->above_median == 0)
+			current->push_cost = len_stack_1 - current->current_index;
+		if (current->target->above_median)
+			current->push_cost += current->target->current_index;
+		else if (current->target->above_median == 0)
+			current->push_cost += len_stack_2 - current->target->current_index;
+		current = current->next;
+	}
+	current->push_cost = len_stack_1 - current->current_index;
+	if (current->target->above_median)
+		current->push_cost += current->target->current_index;
+	else if (current->target->above_median == 0)
+		current->push_cost += len_stack_2 - current->target->current_index;
+	return ;
+}
+
+t_stack	*find_to_push(t_stack **stack)
+{
+	t_stack	*cheapest_node;
+	t_stack	*current;
+
+	cheapest_node = *stack;
+	current = *stack;
+	cheapest = INT_MAX;
+	while (current->next != *stack)
+	{
+		if (current->push_cost < cheapest_node->push_cost)
+			cheapest_node = current;
+		current = current->next;
+	}
+	if (current->push_cost < cheapest_node->push_cost)
+		cheapest_node = current;
+	return (cheapest_node);
 }
