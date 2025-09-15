3df53b18ef7b47bf3fb124757a28e57af72e3957
asando <alexando17@yahoo.com>
2025-09-15 14:54:04 +0200

    removing merge_sort (this is part of radix_sort) radix_sort is not used anymore

--- includes/checker_bonus.h
+++ includes/checker_bonus.h
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   checker_bonus.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/07 17:14:57 by asando            #+#    #+#             */
-/*   Updated: 2025/09/15 14:08:47 by asando           ###   ########.fr       */
+/*   Created: 2025/09/15 14:51:58 by asando            #+#    #+#             */
+/*   Updated: 2025/09/15 14:52:00 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
 
-int	main(int argc, char **argv)
-{
-	t_stack	*stack_1;
-	t_stack	*stack_2;
-
-	stack_1 = NULL;
-	stack_2 = NULL;
-	if (grab_arg(argc, argv, &stack_1) == 0)
-	{
-		if (turk_algo(&stack_1, &stack_2) == -1)
-			write(2, "Error!, Stack not sorted\n", 25);
-	}
-	else
-		write(2, "Error!\n", 7);
-	if (stack_1)
-		stack_clean(&stack_1);
-	return (0);
-}
--- src/bonus/checker_bonus.c
+++ src/bonus/checker_bonus.c
@@ -1,28 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   merge_sort.h                                       :+:      :+:    :+:   */
+/*   checker.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/24 20:27:06 by asando            #+#    #+#             */
-/*   Updated: 2025/08/25 17:32:27 by asando           ###   ########.fr       */
+/*   Created: 2025/09/15 14:51:03 by asando            #+#    #+#             */
+/*   Updated: 2025/09/15 14:51:04 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MERGE_SORT_H
-# define MERGE_SORT_H
-# include <stdlib.h>
 
-typedef struct s_merge
-{
-	int	*arr;
-	int	*left_arr;
-	int	*right_arr;
-	int	left;
-	int	mid;
-	int	right;
-}	t_merge;
-
-int	merge_sort(int *arr, int left, int right);
-#endif
